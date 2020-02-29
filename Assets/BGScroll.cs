a426157f668713ec6925c16a4dbdf9a8d3da7b12
Arjun Kapoor <kaparjun199@gmail.com>
2020-02-29 13:25:43 +0530

    unity assets

---
+++ Assets/BGScroll.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class BGScroll : MonoBehaviour
+{
+    public float scrollSpeed;
+    private MeshRenderer meshRenderer;
+
+    private void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+    }
+
+    private void Update()
+    {
+        float x = Time.time * scrollSpeed;
+        Vector2 offset = new Vector2(x, 0);
+        meshRenderer.sharedMaterial.SetTextureOffset("_MainTex", offset);
+    }
+}
